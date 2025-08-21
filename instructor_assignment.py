fa16290d4717adb70b2eeddeb112b39f3732ac4a
urumuru <joss8129519@gmail.com>
2025-08-21 20:54:44 +0900

    강사님 과제

---
+++ instructor_assignment.py
@@ -0,0 +1,27 @@
+word = "Python"
+# 예상 결과: h
+# 예상 결과: n
+
+print(word[3])
+print(word[5])
+
+numbers = [10, 20, 30, 40, 50]
+# 예상 결과: 30
+# 예상 결과: 10
+
+print(numbers[2])
+print(numbers[0])
+
+text = "Python"
+# 예상 결과: Pyt
+# 예상 결과: thon
+
+print(text[0:3])
+print(text[2:])
+
+data = [10, 20, 30, 40, 50]
+# 예상 결과: [20, 30, 40]
+# 예상 결과: [50, 40, 30, 20, 10]
+
+print(data[1:4])
+print(data[:-1])
